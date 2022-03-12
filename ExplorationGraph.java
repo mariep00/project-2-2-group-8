66dc246c61d7bab92c408a0ec63e0e81acad9b28
Roman20022002 <72268723+Roman20022002@users.noreply.github.com>
2022-03-12 20:47:28 +0100

    Add files via upload

---
+++ ExplorationGraph.java
@@ -0,0 +1,56 @@
+package controller.maps.graph;
+
+import controller.Vector2D;
+import controller.maps.Tile;
+import controller.maps.Tile.Type;
+
+public class ExplorationGraph {
+
+    private Node origin;
+    private Node currentPosition;
+    private GraphAdjacencyList list;
+
+    public ExplorationGraph(){
+        Tile t = new Tile();
+        t.setType(Type.SPAWN_AREA_GUARDS);
+        this.origin = new Node(new Vector2D(0,0), t);
+        list = new GraphAdjacencyList();
+        list.addVertex(origin);
+        this.currentPosition = origin;
+    }
+
+    /**
+     * Creates a vertex by setting its position on the map, its tile type
+     * @param vector Vector2D containing the coordinates x and y
+     * @param type of the Tile
+     * @param hasWall setting if the newly created node has a wall
+     */
+    public void createNode(Vector2D vector, Tile type, boolean hasWall){
+        if(!nodeExists(vector)){
+            Node node =  new Node(vector, type);
+            this.list.addVertex(node);
+            this.list.checkEdges(node);
+            if(hasWall){
+                this.list.addSelfEdge(node);
+            }
+        }
+    }
+
+    /**
+     * Checks if a vertex already exists at a given position on the map
+     * @param vector Vector2D containing the coordinates x and y
+     * @return true if it there is already a node
+     */
+    private boolean nodeExists(Vector2D vector){
+        return this.list.isVisited(vector); //check the neighbours (inside the visited bucket)
+    }
+
+    public Node getCurrentPosition() { return currentPosition; }
+    public void setCurrentPosition(Vector2D vector2D) {
+        this.currentPosition = list.getNode(vector2D);
+    }
+
+    public GraphAdjacencyList getList() {
+        return this.list;
+    }
+}
---
+++ GraphAdjacencyList.java
@@ -0,0 +1,197 @@
+package controller.maps.graph;
+
+import controller.Vector2D;
+
+import java.util.LinkedList;
+
+public class GraphAdjacencyList {
+
+    LinkedList<Node> list;
+    LinkedList<Node> frontiers; //nodes with less than 4 edges;
+
+    public GraphAdjacencyList(){
+        this.list = new LinkedList<>();
+        this.frontiers = new LinkedList<>();
+    }
+
+    /**
+     * Checks if there is an edge from x to y
+     * @param x node
+     * @param y node
+     * @return true if there is an edge from x to y
+     */
+    public boolean adjacent(Node x, Node y){
+        boolean adjacent = false;
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                for(int j=0; j < this.list.get(i).getEdges().size(); j++){
+                    if(this.list.get(i).getEdges().get(j).equals(y))
+                        adjacent = true;
+                }
+            }
+        }
+        return adjacent;
+    }
+
+    /**
+     * Returns all the vertices y s.t. there is an edge from x to y
+     * @param x node
+     * @return a linked list containing the neighboring nodes of x
+     */
+    public LinkedList<Node> neighbors(Node x){
+        LinkedList<Node> neighbors = null;
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                neighbors = this.list.get(i).getEdges();
+            }
+        }
+        return neighbors;
+    }
+
+    /**
+     * Adds the vertex x to the list and potentially to the frontiers (condition on adding x to frontiers?)
+     * @param x node
+     */
+    public void addVertex(Node x){
+        this.list.addFirst(x); //add or addFirst
+        this.frontiers.addFirst(x);
+
+    }
+
+    /**
+     * Removes the vertex x from the list (from frontiers?)
+     * @param x node
+     */
+    public void removeVertex(Node x){
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                while(!this.list.get(i).getEdges().isEmpty()){
+                    removeEdge(this.list.get(i).getEdges().get(0), x);
+                }
+                this.list.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Adds an edge from vertex x to vertex y
+     * @param x node
+     * @param y node
+     */
+    public void addEdge(Node x, Node y){
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                this.list.get(i).addEdge(y);
+                for(int j = 0; j < this.list.size(); j++){
+                    if(this.list.get(j).equals(y)){
+                        this.list.get(j).addEdge(x);
+                    }
+                }
+            }
+        }
+        updateFrontiers(x, y);
+    }
+
+    /**
+     * Adds an edge to the vertex x itself
+     * @param x node
+     */
+    public void addSelfEdge(Node x){
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                this.list.get(i).addEdge(x);
+            }
+        }
+    }
+
+    /**
+     * Removes an edge from vertex x to vertex y
+     * @param x node
+     * @param y node
+     */
+    public void removeEdge(Node x, Node y){
+        for(int i=0; i < this.list.size(); i++){
+            if(this.list.get(i).equals(x)){
+                this.list.get(i).removeEdge(y);
+                for(int j=0; j < this.list.size(); j++){
+                    if(this.list.get(j).equals(y)){
+                        this.list.get(j).removeEdge(x);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if one of the node of the list has already been visited
+     * @param vector positions x and y of the node on the map
+     * @return true if one of the nodes of the list has been visited
+     */
+    public boolean isVisited(Vector2D vector){
+        for(Node node: this.list){
+            if(node.COORDINATES.equals(vector)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks the edges of a newly created node and updates the frontiers w.r.t. the newly created node connections
+     * @param node node
+     */
+    public void checkEdges(Node node){
+        Vector2D[] nodeNeighbours = node.getNeigbours();
+        for(int j = 0; j < frontiers.size(); j++) {
+            Node n = frontiers.get(j);
+            for(int i=0; i < nodeNeighbours.length; i++){
+                if(n.COORDINATES.equals(nodeNeighbours[i])){
+                    addEdge(node, n);
+                }
+            }
+        }
+    }
+
+    /**
+     * Updates the frontiers by removing vertices x and y only if their numbers of edges became bigger or equal to 4 respectively
+     * @param x node
+     * @param y node
+     */
+    private void updateFrontiers(Node x, Node y){
+        if(x.getEdges().size() >= 4){
+            removeVertexFromFrontiers(x);
+        }
+        if(y.getEdges().size() >= 4) {
+            removeVertexFromFrontiers(y);
+        }
+    }
+
+    public void checkFrontierEdges(){
+
+    }
+
+    /**
+     * Removes a specific vertex x from the frontiers
+     * @param x node
+     */
+    public void removeVertexFromFrontiers(Node x){
+        for(int i=0; i < this.frontiers.size(); i++){
+            if(this.frontiers.get(i).equals(x)){
+                this.frontiers.remove(i);
+            }
+        }
+    }
+
+    public Node getNextFrontier() {
+        return frontiers.getFirst();
+    }
+
+    public Node getNode(Vector2D vector2D) {
+        for (Node node : this.list){
+            if (node.COORDINATES.equals(vector2D)){
+                return node;
+            }
+        }
+        return null;
+    }
+}
