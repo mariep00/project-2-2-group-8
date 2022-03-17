01b59705184770d269e27cc98086b757cb56aca2
JohannUM <72266031+JohannUM@users.noreply.github.com>
2022-03-17 12:46:39 +0100

    Fixed vision for map without walls again, merged the graph into one file

--- src/main/java/controller/maps/graph/ExplorationGraph.java
+++ src/main/java/controller/maps/graph/ExplorationGraph.java
@@ -1,6 +1,7 @@
 package controller.maps.graph;
 
 import controller.Vector2D;
+import controller.maps.HashMap;
 import controller.maps.Tile;
 import controller.maps.Tile.Type;
 
@@ -8,17 +9,29 @@ public class ExplorationGraph {
 
     private Node origin;
     private Node currentPosition;
-    private GraphAdjacencyList graphAdjacencyList;
+
+    private HashMap nodes; //all nodes
+    private HashMap frontiers; //nodes with less than 4 edges;
 
     public ExplorationGraph(){
+        this.nodes = new HashMap(4000);
+        this.frontiers = new HashMap(1000);
         Tile t = new Tile();
         t.setType(Type.SPAWN_AREA_GUARDS);
         this.origin = new Node(new Vector2D(0,0), t);
-        graphAdjacencyList = new GraphAdjacencyList();
-        graphAdjacencyList.addVertex(origin);
+        addVertex(origin);
         this.currentPosition = origin;
     }
 
+    /**
+     * Returns the Node with the specified Vector2D
+     * @param vector
+     * @return Node with the specified Vector2D
+     */
+    public Node getNode(Vector2D vector) {
+        return nodes.getValue(vector);
+    }
+    
     /**
      * Creates a vertex by setting its position on the map, its tile type
      * @param vector Vector2D containing the coordinates x and y
@@ -27,18 +40,23 @@ public class ExplorationGraph {
     public Node createNode(Vector2D vector, Tile type){
         if (!nodeExists(vector)) {
             Node node = new Node(vector, type);
-            this.graphAdjacencyList.addVertex(node);
-            this.graphAdjacencyList.checkEdges(node);
+            addVertex(node);
+            checkEdges(node);
             return node;
         }
         return null;
     }
 
-    public void addWall(Vector2D pos, int direction) {
-        Node node = this.graphAdjacencyList.getNode(pos);
+    /**
+     * Adds an edge to itself to represent a wall in the specified direction
+     * @param vector Vector2D of the node adjacent to the wall
+     * @param direction direction of the wall from the vector
+     */
+    public void addWall(Vector2D vector, int direction) {
+        Node node = getNode(vector);
         if (node != null)  {
-            this.graphAdjacencyList.addSelfEdge(node, direction);
-            this.graphAdjacencyList.checkEdges(node);
+            addSelfEdge(node, direction);
+            checkEdges(node);
         }
         
     }
@@ -46,30 +64,100 @@ public class ExplorationGraph {
     /**
      * Checks if a vertex already exists at a given position on the map
      * @param vector Vector2D containing the coordinates x and y
-     * @return true if it there is already a node
+     * @return true if the Node exists
      */
     private boolean nodeExists(Vector2D vector){
-        return this.graphAdjacencyList.isVisited(vector);
+        return nodes.getValue(vector) != null;
     }
 
     public Node getCurrentPosition() { return currentPosition; }
     public void setCurrentPosition(Vector2D vector2D) {
-        this.currentPosition = graphAdjacencyList.getNode(vector2D);
+        this.currentPosition = getNode(vector2D);
     }
+
+    /**
+     * Adds an edge from node1 to node2, but not the other way around
+     * @param node1
+     * @param node2
+     */
     public void addDirectedEdge(Node node1, Node node2) {
-        graphAdjacencyList.addDirectedEdge(node1, node2);
+        node1.addEdge(node2);
+        updateFrontiers(node1);
     }
 
-    public Node getNode(Vector2D vector) {
-        return graphAdjacencyList.getNode(vector);
-    }
-
+    /**
+     * Checks if the teleport edge already exists
+     * @param node1 Node of the teleporter (entrance)
+     * @param node2 Node of the destination of the teleport (exit)
+     * @return true if the edge already exists
+     */
     public boolean teleportEdgeExistsBetween(Node node1, Node node2) {
         return node1.getEdges()[4] != null && node1.getEdges()[4].equals(node2);
     }
 
+    /**
+     * Adds the vertex x to the list and potentially to the frontiers (condition on adding x to frontiers?)
+     * @param x node
+     */
+    public void addVertex(Node x){
+        this.nodes.addEntry(x.COORDINATES, x); //add or addFirst
+        this.frontiers.addEntry(x.COORDINATES, x);
+    }
+
+    /**
+     * Adds an edge from node1 to node2, and the other way around
+     * @param node1 node
+     * @param node2 node
+     */
+    public void addUndirectedEdge(Node node1, Node node2){
+        addDirectedEdge(node1, node2);
+        addDirectedEdge(node2, node1);
+    }
+
+    /**
+     * Adds an edge to the vertex x itself
+     * @param x node
+     */
+    public void addSelfEdge(Node x, int direction){
+        x.addSelfEdge(direction);
+        updateFrontiers(x);
+    }
+
+    /**
+     * Checks if one of the node of the list has already been visited
+     * @param vector positions x and y of the node on the map
+     * @return true if one of the nodes of the list has been visited
+     */
+    public boolean isVisited(Vector2D vector){
+        return nodes.getValue(vector) != null;
+    }
+
+    /**
+     * Checks the edges of a newly created node and updates the frontiers w.r.t. the newly created node connections
+     * @param node node
+     */
+    public void checkEdges(Node node){
+        Vector2D[] nodeNeighbours = node.getNeigbours();
+        for (int i=0; i < nodeNeighbours.length; i++) {
+            Node nodeNeighbour = frontiers.getValue(nodeNeighbours[i]);
+            if (nodeNeighbour != null) {
+                addUndirectedEdge(node, nodeNeighbour);
+            }
+        }
+    }
+
+    /**
+     * Updates the frontiers by removing vertices x and y only if their numbers of edges became bigger or equal to 4 respectively
+     * @param x node
+     */
+    private void updateFrontiers(Node x){
+        if (x.getNumberOfEdges() >= 4) {
+            frontiers.removeEntry(x.COORDINATES);
+        }
+    }
+
     @Override
     public String toString() {
-        return graphAdjacencyList.toString();
+        return "Number of nodes: " + nodes.getNumberOfNodes() + " Size Frontier: " + frontiers.getNumberOfNodes();
     }
 }
