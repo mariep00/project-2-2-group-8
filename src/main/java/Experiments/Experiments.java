ba74554ce62d752908978e30f857ccc821e983ba
Joaquin <j.monederosimon@student.maastrichtuniversity.nl>
2022-03-22 15:13:38 +0100

    Praying for this to work
    (maybe) I made a mess

--- src/main/java/Experiments/Experiments.java
+++ src/main/java/Experiments/Experiments.java
@@ -5,14 +5,20 @@ import controller.MapBuilder;
 import launchers.Launcher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Experiments {
 
     public static final int ITERATIONS = 10;
     private static int gameCounter =0;
+    private static double totalTime =0;
+    private static PrintWriter out;
+
 
     public static Controller controller;
     private static final boolean DEBUG = true;
+    private static final boolean saveResults = true;
 
     private static String path1 = "mapGiacomo,90D.txt";
     private static String path2 = "mapGiacomo,135D.txt";
@@ -30,9 +36,15 @@ public class Experiments {
     }
 
     public static void main(String[] args) {
+        if (saveResults) createFile();
 
         currentBrain = 1;
         //random 90D, map Giacomo
+        out.println("Iterarions: " + gameCounter);
+        //random 90D
+        out.println("------------------");
+        out.println("random 90D");
+        totalTime = 0;
         for (int i=0; i < ITERATIONS; i++) {
 
 
@@ -40,14 +52,16 @@ public class Experiments {
 
             gameCounter ++;
 
-            System.out.println();
-
-            if (DEBUG)System.out.println("#########GAME " + gameCounter +" ############");
-            System.out.println();
-
+            out.println(gameCounter + ".    Time taken: "+ controller.time);
         }
+        out.println("Iterations: " + gameCounter);
+        out.println(" ---- AVG TIME: " + (totalTime/gameCounter));
+
+        //random 135D
+        out.println("------------------");
+        out.println("random 135D");
+        totalTime = 0;
         currentBrain = 1;
-        //random 135D, map Giacomo
         for (int i=0; i < ITERATIONS; i++) {
 
 
@@ -55,15 +69,17 @@ public class Experiments {
 
             gameCounter ++;
 
-            System.out.println();
-
-            if (DEBUG)System.out.println("#########GAME " + gameCounter +" ############");
-            System.out.println();
-
+            out.println(gameCounter + ".    Time taken: "+ controller.time);
         }
+        out.println("Iterations: " + gameCounter);
+        out.println(" ---- AVG TIME: " + (totalTime/gameCounter));
+
 
         currentBrain = 1;
         //random 180D, map Giacomo
+        out.println("------------------");
+        out.println("random 180D");
+        totalTime = 0;
         for (int i=0; i < ITERATIONS; i++) {
 
 
@@ -71,36 +87,26 @@ public class Experiments {
 
             gameCounter ++;
 
-            System.out.println();
-
-            if (DEBUG)System.out.println("#########GAME " + gameCounter +" ############");
-            System.out.println();
-
+            out.println(gameCounter + ".    Time taken: "+ controller.time);
+            totalTime = totalTime + controller.time;
         }
-
-        currentBrain = 2;
-        //frontier 90D, map Giacomo
-        for (int i=0; i < ITERATIONS; i++) {
-
-
-            Experiments experiments = new Experiments(path1);
-
-            gameCounter ++;
-
-            System.out.println();
-
-            if (DEBUG)System.out.println("#########GAME " + gameCounter +" ############");
-            System.out.println();
-
-        }
-
-
-
-
-
-
-
+        out.println("Iterations: " + gameCounter);
+        out.println(" ---- AVG TIME: " + (totalTime/gameCounter));
+        
     }
+
+    public static PrintWriter createFile(){
+        String outputFileName = "experiments.txt";
+        out = null;
+        try {
+            out = new PrintWriter(outputFileName);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
 
     }
 
