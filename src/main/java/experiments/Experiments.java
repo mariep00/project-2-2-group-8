7c7ad5f60ecbdc2f51e5f8f7611771fdf44adb0f
Yannick Damoiseaux <yannick.damoiseaux@student.maastrichtuniversity.nl>
2022-05-17 10:33:52 +0200

    Multithreaded

--- src/main/java/experiments/Experiments.java
+++ src/main/java/experiments/Experiments.java
@@ -2,15 +2,17 @@ package experiments;
 
 import gamelogic.agent.tasks.TaskContainer;
 import gamelogic.agent.tasks.general.ExplorationTaskFrontier;
-import gamelogic.controller.Controller;
-import gamelogic.maps.MapBuilder;
 import gamelogic.controller.endingconditions.EndingExploration;
 import gamelogic.controller.gamemodecontrollers.ControllerExploration;
+import gamelogic.maps.MapBuilder;
 import gamelogic.maps.ScenarioMap;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The three different files are meant to be used on a map where some parameters are changed each time.
@@ -25,12 +27,12 @@ public class Experiments {
     private static double totalTime =0;
     private static PrintWriter out;
 
-
-    public static Controller controller;
     private static final boolean DEBUG = true;
     private static final boolean saveResults = true;
 
-    private static String path1 = "C://Users//giaco//Downloads//map1.txt";
+    //private static String path1 = "C://Users//giaco//Downloads//map1.txt";
+    private static String path1 = "/Users/Yannick/Documents/DKE_UM/Year_2/Project_2-2/code/src/main/resources/maps/Map1.txt";
+
     //private static String path1 = "testmap.txt";
     //private static String path2 = "/Users/Johan/Documents/GitHub/project-2-2-group-8/src/main/resources/maps/testmap.txt";
     //private static String path3 = "/Users/Johan/Documents/GitHub/project-2-2-group-8/src/main/resources/maps/testmap.txt";
@@ -40,31 +42,38 @@ public class Experiments {
 
     // 1 random
     // 2 frontier
-    public Experiments(String path){
-        ScenarioMap scenarioMap = new MapBuilder(new File(path)).getMap();
-        controller = new ControllerExploration(scenarioMap, new EndingExploration(scenarioMap), new TaskContainer(new ExplorationTaskFrontier()));
+    public Experiments(ControllerExploration controller){
         controller.init();
         controller.engine();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if (saveResults) {
             createFile();
             if (DEBUG) System.out.println("File created?");
         }
 
-
         currentBrain = 1;
         for (int a=1; a<=RUNS; a++) {
             totalTime = 0;
             gameCounter = 0;
+            ThreadPoolExecutor threadPool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), 50, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
             for (int i = 0; i < ITERATIONS; i++) {
+                int finalI = i;
+                int finalA = a;
+                threadPool.submit(() -> {
+                    ScenarioMap scenarioMap = new MapBuilder(new File(path1)).getMap();
+                    ControllerExploration controller = new ControllerExploration(scenarioMap, new EndingExploration(scenarioMap), new TaskContainer(new ExplorationTaskFrontier()));
 
-                Experiments experiments = new Experiments(path1);
-                gameCounter++;
-                System.out.println(gameCounter + ".    Time taken: " + controller.time);
-                totalTime = totalTime + controller.time;
+                    Experiments experiments = new Experiments(controller);
+                    gameCounter++;
+                    System.out.println(finalA + ", " + finalI);
+                    //System.out.println(gameCounter + ".    Time taken: " + controller.time);
+                    totalTime = totalTime + controller.time;
+                });
             }
+            threadPool.shutdown();
+            threadPool.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
             out.println("RUN:" + a);
             out.println(" ---- AVG TIME: " + (totalTime / gameCounter));
         }
