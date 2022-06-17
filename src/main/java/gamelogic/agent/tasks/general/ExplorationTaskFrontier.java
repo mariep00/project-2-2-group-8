35de8f320a6da45a381f20cc7981634f257a94a2
JohannUM <72266031+JohannUM@users.noreply.github.com>
2022-06-17 02:02:19 +0200

    Fixed Evasion

--- src/main/java/gamelogic/agent/tasks/general/ExplorationTaskFrontier.java
+++ src/main/java/gamelogic/agent/tasks/general/ExplorationTaskFrontier.java
@@ -38,7 +38,7 @@ public class ExplorationTaskFrontier implements TaskInterface {
     @Override
     public int performTask(ExplorationGraph graph, double orientation, double pheromoneMarkerDirection, List<Sound> sounds, VisionMemory[] guardsSeen, VisionMemory[] intrudersSeen) {
         if (futureMoves == null || futureMoves.isEmpty()) {
-            System.out.println("New moves");
+            //System.out.println("New moves");
             finished = false;
             futureMoves = new Stack<>();
             this.orientation = orientation;
@@ -51,10 +51,10 @@ public class ExplorationTaskFrontier implements TaskInterface {
                 
                 foundReachableNode = moveTo();
                 if (!foundReachableNode) {
-                    System.out.println("----- Didnt find reachable Node: " + frontierIndexToGoTo);
+                    //System.out.println("----- Didnt find reachable Node: " + frontierIndexToGoTo);
                     frontierIndexToGoTo++;
                     if (goalNode == lastNode) {
-                        System.out.println("HMMM");
+                        //System.out.println("HMMM");
                         whenStuck();
                     }
 
